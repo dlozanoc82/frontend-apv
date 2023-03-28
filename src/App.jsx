fee2657d0584d76b0a8c15086cad3e5dff56f2eb
Daniel <dlozanoc862@gmail.com>
2023-03-28 13:51:47 -0500

    :sparkles: Estableciendo rutas y archivos para la Autenticacion

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,10 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import AuthLayout from "./layout/AuthLayout";
 import Login from "./pages/Login";
+import Registrar from "./pages/Registrar";
+import ConfirmarCuenta from "./pages/ConfirmarCuenta";
+import ResetPassword from "./pages/ResetPassword";
+
 
 //Component 
 function App() {
@@ -9,6 +13,9 @@ function App() {
             <Routes>
                 <Route path="/" element={<AuthLayout />}>
                     <Route index element={<Login />} />
+                    <Route path="registrar" element={<Registrar />} />
+                    <Route path="confirmar/:id" element={<ConfirmarCuenta />} />
+                    <Route path="reset-password" element={<ResetPassword />} />
                 </Route>
             </Routes>
         </BrowserRouter>
---
+++ src/pages/ConfirmarCuenta.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ConfirmarCuenta = () => {
+    return (
+        <>
+            <h1>Desde ConfirmarCuenta</h1>
+        </>
+    )
+}
+
+export default ConfirmarCuenta
