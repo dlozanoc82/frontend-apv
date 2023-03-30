f323235b3549a00213a56983c292e58a16ebac25
Daniel <dlozanoc862@gmail.com>
2023-03-30 12:12:27 -0500

    :lipstick: Se agregan estilos a las alertas dinamicas

--- src/pages/Registrar.jsx
+++ src/pages/Registrar.jsx
@@ -1,5 +1,6 @@
 import { Link } from "react-router-dom";
 import { useState } from "react";
+import Alerta from "../components/Alerta";
 
 const Registrar = () => {
 
@@ -7,24 +8,31 @@ const Registrar = () => {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('')
     const [repetirPassword, setRepetirPassword] = useState('');
+    const [alerta, setAlerta] = useState('')
 
     const handleSubmit = e => {
         e.preventDefault();
 
         if ([nombre, email, password, repetirPassword].includes('')) {
-            console.log('Hay campos vacios')
+            setAlerta({msg: 'Hay campos vacios', error: true});
             return;
         }
 
         if (password !== repetirPassword) {
-            console.log('Los passwords no son iguales');
+            setAlerta({msg: 'Los passwords no son iguales', error: true});
+            return;
         }
 
         if (password.length < 6) {
-            console.log('Password es muy corto, agrega minimo 6 caracteres');
+            setAlerta({msg: 'Password es muy corto, agrega minimo 6 caracteres', error: true});
+            return;
         }
+    
+        setAlerta({})
 
     }
+
+    const {msg} = alerta;
 
     return (
         <>
@@ -36,6 +44,11 @@ const Registrar = () => {
             </div>
 
             <div className="mt-20 md:mt-5 shadow-lg px-5 py-10 rounded-xl bg-white">
+
+                {msg &&<Alerta 
+                    alerta={alerta}
+                />}
+
                 <form onSubmit={handleSubmit}>
                     <div className='my-5'>
                         <label className='uppercase text-gray-600 block text-xl font-bold'>
