ad9844fc8a0c1c178e068732c634512f440b3802
Daniel <dlozanoc862@gmail.com>
2023-03-30 10:55:01 -0500

    :lipstick: Se crea y agrega estilos al componente Login

--- src/layout/AuthLayout.jsx
+++ src/layout/AuthLayout.jsx
@@ -3,8 +3,9 @@ import { Outlet } from "react-router-dom";
 const AuthLayout = () => {
     return (
         <>
-            <h1>Desde AuthLayout</h1>
-            <Outlet />
+            <main className='container mx-auto md:grid md:grid-cols-2 mt-14 gap-10 p-5 items-center'>
+                <Outlet />
+            </main>
         </>
     )
 }
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,10 +1,42 @@
-import React from 'react'
+import { Link } from "react-router-dom";
 
 const Login = () => {
     return (
         <>
-            <h2>Login</h2>
-            
+            <div>
+                <h1 className='text-indigo-600 font-black text-6xl'>
+                    Inicia Sesión y Administra tus <span className='text-black'>Pacientes</span>
+                </h1>
+            </div>
+            <div>
+                <form>
+                    <div className='my-5'>
+                        <label className='uppercase text-gray-600 block text-xl font-bold'>
+                            Email
+                        </label>
+                        <input className='border w-full p-3 mt-3 bg-gray-50 rounded-xl' type="text" placeholder='Email de Registro' />
+                    </div>
+                    <div className='my-5'>
+                        <label className='uppercase text-gray-600 block text-xl font-bold'>
+                            Password
+                        </label>
+                        <input className='border w-full p-3 mt-3 bg-gray-50 rounded-xl' type="password" placeholder='Tu Password' />
+                    </div>
+                    <input 
+                        type="submit" 
+                        value="Iniciar Sesión" 
+                        className='bg-indigo-700 w-full py-3 px-10 rounded-xl text-white uppercase font-bold mt-5 hover:cursor-pointer hover:bg-indigo-800 md:w-auto'
+                    />
+                </form>
+                <nav className="mt-10 lg:flex lg:justify-between">
+                    <Link  className="block text-center my-5 text-gray-500" to='/registrar'>
+                        ¿No tienes una cuenta? Registrate
+                    </Link>
+                    <Link className="block text-center my-5 text-gray-500" to='/reset-password'>
+                        Olvide mi contraseña
+                    </Link>
+                </nav>
+            </div>
         </>
     )
 }
