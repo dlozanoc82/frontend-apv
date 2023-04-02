aca646bc94bab6ea6e2cc7e74910686f913c256d
Daniel <dlozanoc862@gmail.com>
2023-04-02 14:40:59 -0500

    :sparkles: Se crea el context para la autenticacion

--- src/App.jsx
+++ src/App.jsx
@@ -5,20 +5,23 @@ import Registrar from "./pages/Registrar";
 import ConfirmarCuenta from "./pages/ConfirmarCuenta";
 import ResetPassword from "./pages/ResetPassword";
 import NuevoPassword from "./pages/NuevoPassword";
+import { AuthProvider } from "./context/AuthProvider";
 
 //Component 
 function App() {
     return (
         <BrowserRouter>
-            <Routes>
-                <Route path="/" element={<AuthLayout />}>
-                    <Route index element={<Login />} />
-                    <Route path="registrar" element={<Registrar />} />
-                    <Route path="confirmar/:id" element={<ConfirmarCuenta />} />
-                    <Route path="reset-password" element={<ResetPassword />} />
-                    <Route path="reset-password/:token" element={<NuevoPassword />} />
-                </Route>
-            </Routes>
+            <AuthProvider>
+                <Routes>
+                    <Route path="/" element={<AuthLayout />}>
+                        <Route index element={<Login />} />
+                        <Route path="registrar" element={<Registrar />} />
+                        <Route path="confirmar/:id" element={<ConfirmarCuenta />} />
+                        <Route path="reset-password" element={<ResetPassword />} />
+                        <Route path="reset-password/:token" element={<NuevoPassword />} />
+                    </Route>
+                </Routes>
+            </AuthProvider>
         </BrowserRouter>
     )
 }
