327ad720437e4483e3bfd68a5915f7a0b2709111
Daniel <dlozanoc862@gmail.com>
2023-04-02 15:22:03 -0500

    :sparkles: Autenticación de Usuarios Login

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,10 +1,39 @@
 import { Link } from "react-router-dom";
+import { useState } from "react";
 import useAuth from "../hooks/useAuth";
+import Alerta from "../components/Alerta";
+import clienteAxios from "../config/axios";
 
 const Login = () => {
 
-    const {auth} = useAuth();
-    console.log(auth);
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [alerta, setAlerta] = useState({});
+
+    const handleSubmit = async(e) => {
+        e.preventDefault();
+
+        if ([email, password].includes('')) {
+            setAlerta({
+                msg: 'Todos los campos son obligatorios',
+                error: true
+            });
+            return;
+        }
+
+        try {
+            const { data } = await clienteAxios.post('/veterinarios/login', {email, password});
+            localStorage.setItem('token', data.token);
+        } catch (error) {
+            setAlerta({
+                msg: error.response.data.msg,
+                error: true
+            })
+        }
+
+    }
+
+    const {msg} = alerta;
 
     return (
         <>
@@ -13,19 +42,38 @@ const Login = () => {
                     Inicia Sesión y Administra tus <span className='text-black'>Pacientes</span>
                 </h1>
             </div>
+            
             <div className="mt-20 md:mt-5 shadow-lg px-5 py-10 rounded-xl bg-white">
-                <form>
+                {msg && <Alerta alerta={alerta}/>}
+                
+                <form onSubmit={handleSubmit}>
                     <div className='my-5'>
-                        <label className='uppercase text-gray-600 block text-xl font-bold'>
+                        <label 
+                            className='uppercase text-gray-600 block text-xl font-bold'
+                        >
                             Email
                         </label>
-                        <input className='border w-full p-3 mt-3 bg-gray-50 rounded-xl' type="email" placeholder='Email de Registro' />
+                        <input 
+                            className='border w-full p-3 mt-3 bg-gray-50 rounded-xl' type="email" 
+                            placeholder='Email de Registro' 
+                            value={email} 
+                            onChange={ e => setEmail(e.target.value)}
+                        />
                     </div>
                     <div className='my-5'>
-                        <label className='uppercase text-gray-600 block text-xl font-bold'>
+                        <label 
+                            className='uppercase text-gray-600 block text-xl font-bold'
+                        >
                             Password
                         </label>
-                        <input className='border w-full p-3 mt-3 bg-gray-50 rounded-xl' type="password" placeholder='Tu Password' />
+
+                        <input 
+                            className='border w-full p-3 mt-3 bg-gray-50 rounded-xl'
+                            type="password" 
+                            placeholder='Tu Password' 
+                            value={password} 
+                            onChange={ e => setPassword(e.target.value)}
+                        />
                     </div>
                     <input 
                         type="submit" 
@@ -33,6 +81,7 @@ const Login = () => {
                         className='bg-indigo-700 w-full py-3 px-10 rounded-xl text-white uppercase font-bold mt-5 hover:cursor-pointer hover:bg-indigo-800 md:w-auto'
                     />
                 </form>
+                
                 <nav className="mt-10 lg:flex lg:justify-between">
                     <Link  className="block text-center my-5 text-gray-500" to='/registrar'>
                         ¿No tienes una cuenta? Registrate
