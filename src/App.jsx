6061b1d287f81f69202912916ff79851716dc493
Daniel <dlozanoc862@gmail.com>
2023-04-09 16:38:54 -0500

    :sparkles: Se crea el context para los pacientes y se agrega su configuracion

--- src/App.jsx
+++ src/App.jsx
@@ -6,28 +6,31 @@ import Registrar from "./pages/Registrar";
 import ConfirmarCuenta from "./pages/ConfirmarCuenta";
 import ResetPassword from "./pages/ResetPassword";
 import NuevoPassword from "./pages/NuevoPassword";
-import { AuthProvider } from "./context/AuthProvider";
 import AdministrarPacientes from "./pages/AdministrarPacientes";
 
+import { AuthProvider } from "./context/AuthProvider";
+import { PacientesProvider } from "./context/PacientesProvider";
 
 //Component 
 function App() {
     return (
         <BrowserRouter>
             <AuthProvider>
-                <Routes>
-                    <Route path="/" element={<AuthLayout />}>
-                        <Route index element={<Login />} />
-                        <Route path="registrar" element={<Registrar />} />
-                        <Route path="confirmar/:id" element={<ConfirmarCuenta />} />
-                        <Route path="reset-password" element={<ResetPassword />} />
-                        <Route path="reset-password/:token" element={<NuevoPassword />} />
-                    </Route>
+                <PacientesProvider>
+                    <Routes>
+                        <Route path="/" element={<AuthLayout />}>
+                            <Route index element={<Login />} />
+                            <Route path="registrar" element={<Registrar />} />
+                            <Route path="confirmar/:id" element={<ConfirmarCuenta />} />
+                            <Route path="reset-password" element={<ResetPassword />} />
+                            <Route path="reset-password/:token" element={<NuevoPassword />} />
+                        </Route>
 
-                    <Route path="/admin"  element={<RutaProtegida/>}>
-                        <Route index element={<AdministrarPacientes/>}></Route>
-                    </Route>
-                </Routes>
+                        <Route path="/admin"  element={<RutaProtegida/>}>
+                            <Route index element={<AdministrarPacientes/>}></Route>
+                        </Route>
+                    </Routes>
+                </PacientesProvider>
             </AuthProvider>
         </BrowserRouter>
     )
---
+++ src/context/PacientesProvider.jsx
@@ -0,0 +1,18 @@
+import { createContext, useState, useEffect } from "react";
+import clienteAxios from "../config/axios";
+
+const PacientesContext = createContext();
+
+export const PacientesProvider = ({children}) =>{
+    return(
+        <PacientesContext.Provider
+            value={{
+                
+            }}
+        >
+            {children}
+        </PacientesContext.Provider>
+    )
+}
+
+export default PacientesContext;
