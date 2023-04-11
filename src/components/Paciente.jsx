eebf81632bc35807ad595e45af5479254ba37e2e
Daniel <dlozanoc862@gmail.com>
2023-04-11 12:18:21 -0500

    :sparkles: Add botones editar y eliminar coponente Paciente

--- src/components/Paciente.jsx
+++ src/components/Paciente.jsx
@@ -29,6 +29,17 @@ const Paciente = ({paciente}) => {
             <p className="font-bold uppercase my-2 text-indigo-700">Sintomas:{' '}
                 <span className="font-normal normal-case text-black">{sintomas}</span>
             </p>
+
+            <div className="flex justify-between my-5">
+                <button
+                    type="button"
+                    className="py-2 px-10 bg-indigo-600 hover:bg-indigo-700 text-white uppercase font-bold rounded-lg"
+                >Editar</button>
+                <button
+                    type="button"
+                    className="py-2 px-10 bg-red-600 hover:bg-red-700 text-white uppercase font-bold rounded-lg"
+                >Eliminar</button>
+            </div>
         </div>
     )
 }
