c6b7de96a2d1f327a3009cee0ed78d9baf89ff3d
Daniel <dlozanoc862@gmail.com>
2023-04-13 23:15:47 -0500

    :sparkles: Se crea el formulario para editar perfil

--- src/pages/EditarPerfil.jsx
+++ src/pages/EditarPerfil.jsx
@@ -1,6 +1,16 @@
+import { useEffect, useState } from "react";
 import AdminNav from "../components/AdminNav";
+import useAuth from "../hooks/useAuth";
 
 const EditarPerfil = () => {
+
+    const { auth } = useAuth();
+    const [perfil, setPerfil] = useState({});
+
+    useEffect(() =>{
+        setPerfil(auth);
+    }, [auth])
+
     return (
         <>
             <AdminNav />
@@ -9,6 +19,53 @@ const EditarPerfil = () => {
             <p className="text-xl mt-5 mb-10 text-center">Modifica tu {' '}
                 <span className="text-indigo-600 font-bold">Información</span>
             </p>
+
+            <div className="flex justify-center">
+                <div className="w-full md:w-1/2 bg-white shadow rounded-lg p-5">
+                    <form>
+                        <div className="my-3">
+                            <label className="uppercase font-bold text-gray-600">Nombre</label>
+                            <input 
+                                type="text"
+                                className="border bg-gray-50 w-full p-2 mt-5 rounded-lg"
+                                name="nombre"
+                            />
+                        </div>
+                        <div className="my-3">
+                            <label className="uppercase font-bold text-gray-600">Sitio Web</label>
+                            <input 
+                                type="text"
+                                className="border bg-gray-50 w-full p-2 mt-5 rounded-lg"
+                                name="web"
+                            />
+                        </div>
+                        <div className="my-3">
+                            <label className="uppercase font-bold text-gray-600">Telefono</label>
+                            <input 
+                                type="text"
+                                className="border bg-gray-50 w-full p-2 mt-5 rounded-lg"
+                                name="telefono"
+                            />
+                        </div>
+                        <div className="my-3">
+                            <label className="uppercase font-bold text-gray-600">Email</label>
+                            <input 
+                                type="email"
+                                className="border bg-gray-50 w-full p-2 mt-5 rounded-lg"
+                                name="email"
+                            />
+                        </div>
+
+                        <input 
+                            type="submit"
+                            value="Guardar Cambios"
+                            className="bg-indigo-700 px-10 py-3 font-bold text-white rounded-lg uppercase w-full mt-5"
+                        />
+
+                    </form>
+                </div>
+            </div>
+
         </>
     )
 }
