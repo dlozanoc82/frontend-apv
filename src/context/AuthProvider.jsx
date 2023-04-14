fd65578b174ff0cb5e9f809338cbbcde97b21e22
Daniel <dlozanoc862@gmail.com>
2023-04-13 23:53:05 -0500

    :sparkles: Enviado datos hacia la API editar perfil

--- src/context/AuthProvider.jsx
+++ src/context/AuthProvider.jsx
@@ -44,8 +44,28 @@ const AuthProvider = ({children}) => {
         setAuth({});
     }
 
-    const actualizarPerfil = datos => {
-        console.log(datos);
+    const actualizarPerfil = async (datos) => {
+        const token = localStorage.getItem('apv_token_practice');
+        if (!token) {
+            setCargando(false);
+            return;
+        };
+            
+        const config = {
+            headers: {
+                "Content-Type": "application/json",
+                Authorization: `Bearer ${token}`
+            }
+        }
+
+        try {
+            const url = `/veterinarios/perfil/${datos._id}`;
+            const {data} = await clienteAxios.put(url, datos, config);
+            console.log(data);
+        } catch (error) {
+            console.log(error.response)
+        }
+
     }
 
     return (
